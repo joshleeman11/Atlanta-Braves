cac38b41032afd741d58acd197d96ebb1df3ad9c
Josh Leeman <joshleeman@joshs-mbp-2.mynetworksettings.com>
2024-10-15 00:39:16 -0400

    color by, date options

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,13 +7,15 @@ import {
     Button,
     Grid,
 } from "@mui/material";
+import DatePicker from "react-multi-date-picker";
 
 const App = () => {
     const [selectedOutcomes, setSelectedOutcomes] = useState([]);
     const [exitSpeedRange, setExitSpeedRange] = useState([20, 130]);
     const [launchAngleRange, setLaunchAngleRange] = useState([-100, 100]);
-    const [batterName, setBatterName] = useState("");
-    const [pitcherName, setPitcherName] = useState("");
+    const [batterNames, setBatterNames] = useState("");
+    const [pitcherNames, setPitcherNames] = useState("");
+    const [dates, setDates] = useState([]);
     const [image, setImage] = useState(null);
     const [loading, setLoading] = useState(false);
 
@@ -37,21 +39,68 @@ const App = () => {
     };
 
     const handleBatterNameChange = (event) => {
-        setBatterName(event.target.value);
+        setBatterNames(event.target.value);
     };
 
     const handlePitcherNameChange = (event) => {
-        setPitcherName(event.target.value);
+        setPitcherNames(event.target.value);
+    };
+
+    const handleDatesChange = (event) => {
+        const formattedDates = new Set();
+        const dates = event;
+
+        const addDateRange = (startDate, endDate) => {
+            let currentDate = new Date(
+                startDate.year,
+                startDate.month.number - 1,
+                startDate.day
+            );
+            const end = new Date(
+                endDate.year,
+                endDate.month.number - 1,
+                endDate.day
+            );
+
+            while (currentDate <= end) {
+                formattedDates.add(
+                    `${
+                        currentDate.getMonth() + 1
+                    }/${currentDate.getDate()}/${currentDate.getFullYear()}`
+                );
+                currentDate.setDate(currentDate.getDate() + 1);
+            }
+        };
+
+        dates.forEach((range) => {
+            if (range.length === 1) {
+                const date = range[0];
+                formattedDates.add(
+                    `${date.month.number}/${date.day}/${date.year}`
+                );
+            } else {
+                addDateRange(range[0], range[1]);
+            }
+        });
+
+        setDates(Array.from(formattedDates));
     };
 
     const handleSubmit = async () => {
         setLoading(true);
+        const batters =
+            batterNames?.split("|").map((batter) => batter.trim()) ?? null;
+        const pitchers =
+            pitcherNames?.split("|").map((pitcher) => pitcher.trim()) ?? null;
         const data = {
             outcomes: selectedOutcomes,
             exitSpeedRange: exitSpeedRange,
             launchAngleRange: launchAngleRange,
-            batterName: batterName,
-            pitcherName: pitcherName
+            batterNames: batters,
+            pitcherNames: pitchers,
+            dates: dates,
+            colorBy: document.querySelector('input[name="colorBy"]:checked')
+                .value,
         };
 
         try {
@@ -76,48 +125,111 @@ const App = () => {
         <Grid container spacing={3} sx={{ padding: "20px" }}>
             {/* Left-side Filters */}
             <Grid item xs={12} md={6}>
-                <h2>Generate Custom Graph</h2>
+                <h2>Baseball Data Visualization</h2>
 
-                <TextField
-                    label="Filter on Batter Name"
-                    variant="outlined"
-                    value={batterName}
-                    onChange={handleBatterNameChange}
-                    fullWidth
-                    margin="normal"
-                    sx={{ width: "80%", marginBottom: "20px" }}
-                />
+                {/* Player Name Filters - Centered */}
+                <div style={{ textAlign: "left" }}>
+                    <Grid container spacing={2} alignItems="center">
+                        <Grid item xs={8}>
+                            <TextField
+                                label="Filter on Batter Name (Lastname, Firstname | Lastname, Firstname)"
+                                variant="outlined"
+                                value={batterNames}
+                                onChange={handleBatterNameChange}
+                                fullWidth
+                                margin="normal"
+                                sx={{ width: "100%" }}
+                            />
+                        </Grid>
+                        <Grid item xs={4}>
+                            <div>
+                                <input
+                                    type="radio"
+                                    id="colorByBatter"
+                                    name="colorBy"
+                                    value="batter_name"
+                                />
+                                <label htmlFor="colorByBatter">
+                                    Color by batter
+                                </label>
+                            </div>
+                        </Grid>
+                    </Grid>
 
-                <TextField
-                    label="Filter on Pitcher Name"
-                    variant="outlined"
-                    value={pitcherName}
-                    onChange={handlePitcherNameChange}
-                    fullWidth
-                    margin="normal"
-                    sx={{ width: "80%", marginBottom: "20px" }}
-                />
+                    {/* Pitcher Name Filter */}
+                    <Grid container spacing={2} alignItems="center">
+                        <Grid item xs={8}>
+                            <TextField
+                                label="Filter on Pitcher Name (Lastname, Firstname | Lastname, Firstname)"
+                                variant="outlined"
+                                value={pitcherNames}
+                                onChange={handlePitcherNameChange}
+                                fullWidth
+                                margin="normal"
+                                sx={{ width: "100%" }}
+                            />
+                        </Grid>
+                        <Grid item xs={4}>
+                            <div>
+                                <input
+                                    type="radio"
+                                    id="colorByPitcher"
+                                    name="colorBy"
+                                    value="pitcher_name"
+                                />
+                                <label htmlFor="colorByPitcher">
+                                    Color by pitcher
+                                </label>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </div>
 
-                <select
-                    multiple
-                    value={selectedOutcomes}
-                    onChange={handleSelectChange}
+                <br />
+
+                {/* Date Picker */}
+                <label htmlFor="datePicker">Filter on date</label>
+                <div style={{ textAlign: "left" }}>
+                    <DatePicker
+                        id="datePicker"
+                        onChange={handleDatesChange}
+                        multiple={true}
+                        range={true}
+                    />
+                </div>
+                <br></br>
+                {/* Outcome Filter & Select - Aligned vertically */}
+                <div
                     style={{
-                        width: "80%",
-                        height: "100px",
-                        marginBottom: "20px",
+                        display: "flex",
+                        flexDirection: "column",
                     }}
                 >
-                    <option value="Single">Single</option>
-                    <option value="Double">Double</option>
-                    <option value="Triple">Triple</option>
-                    <option value="HomeRun">Home Run</option>
-                    <option value="Out">Out</option>
-                    <option value="Error">Error</option>
-                </select>
+                    {/* Outcome Selector */}
+                    <label htmlFor="outcomePicker">Filter on outcome</label>
+                    <select
+                        id="outcomePicker"
+                        multiple
+                        value={selectedOutcomes}
+                        onChange={handleSelectChange}
+                        style={{
+                            width: "20%", // Narrower width
+                            height: "100px",
+                            marginBottom: "20px",
+                        }}
+                    >
+                        <option value="Single">Single</option>
+                        <option value="Double">Double</option>
+                        <option value="Triple">Triple</option>
+                        <option value="HomeRun">Home Run</option>
+                        <option value="Out">Out</option>
+                        <option value="Error">Error</option>
+                    </select>
+                </div>
 
+                {/* Slider for Exit Speed */}
                 <Box sx={{ width: 300, marginTop: "20px" }}>
-                    <Typography gutterBottom>Exit Speed Range</Typography>
+                    <Typography>{`Exit Speed: ${exitSpeedRange[0]} mph to ${exitSpeedRange[1]} mph`}</Typography>
                     <Slider
                         value={exitSpeedRange}
                         onChange={handleExitSpeedChange}
@@ -127,13 +239,11 @@ const App = () => {
                         max={130}
                         step={1}
                     />
-                    <Typography>{`Exit Speed: ${exitSpeedRange[0]} mph to ${exitSpeedRange[1]} mph`}</Typography>
                 </Box>
 
+                {/* Slider for Launch Angle */}
                 <Box sx={{ width: 300, marginTop: "20px" }}>
-                    <Typography gutterBottom>
-                        Launch Angle Range (degrees)
-                    </Typography>
+                    <Typography>{`Launch Angle: ${launchAngleRange[0]}° to ${launchAngleRange[1]}°`}</Typography>
                     <Slider
                         value={launchAngleRange}
                         onChange={handleLaunchAngleChange}
@@ -143,8 +253,40 @@ const App = () => {
                         max={45}
                         step={1}
                     />
-                    <Typography>{`Launch Angle: ${launchAngleRange[0]}° to ${launchAngleRange[1]}°`}</Typography>
                 </Box>
+                <br></br>
+                {/* Color Selector */}
+                <div>
+                    <label>Select how to color the points:</label>
+                    <br />
+
+                    <input
+                        type="radio"
+                        id="colorByOutcome"
+                        name="colorBy"
+                        value="outcome"
+                        defaultChecked
+                    />
+                    <label htmlFor="colorByOutcome">Outcome</label>
+                    <br />
+
+                    <input
+                        type="radio"
+                        id="colorByDate"
+                        name="colorBy"
+                        value="date"
+                    />
+                    <label htmlFor="colorByDate">Date</label>
+                    <br />
+
+                    <input
+                        type="radio"
+                        id="colorByExitSpeed"
+                        name="colorBy"
+                        value="exit_speed"
+                    />
+                    <label htmlFor="colorByExitSpeed">Exit Speed</label>
+                </div>
 
                 <Button
                     onClick={handleSubmit}
@@ -168,7 +310,7 @@ const App = () => {
                     alignItems: "center",
                 }}
             >
-                {image && (
+                {image ? (
                     <img
                         src={`data:image/png;base64,${image}`}
                         alt="Custom Graph"
@@ -179,6 +321,8 @@ const App = () => {
                             objectFit: "contain", // Ensure the image doesn't get cropped
                         }}
                     />
+                ) : (
+                    "No data"
                 )}
             </Grid>
         </Grid>
